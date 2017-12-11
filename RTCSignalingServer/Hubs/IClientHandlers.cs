f24e2adca4d1e66471eaed26f88a130efa57e9cf
Vladyslav <vladkolodka@gmail.com>
2017-12-11 15:40:59 +0200

    Docs

---
+++ RTCSignalingServer/Hubs/IClientHandlers.cs
@@ -0,0 +1,22 @@
+﻿namespace RTCSignalingServer.Hubs
+{
+    public interface IClientHandlers
+    {
+        /// <summary>
+        /// New call offer
+        /// </summary>
+        /// <param name="groupName">Interlocutor group name</param>
+        /// <param name="sdp">SDP pocket</param>
+        void NewOffer(string groupName, string sdp);
+        /// <summary>
+        /// Call answer received
+        /// </summary>
+        /// <param name="sdp">SDP pocket</param>
+        void NewAnswer(string sdp);
+        /// <summary>
+        /// On new candidate received
+        /// </summary>
+        /// <param name="ice">ICE candidate</param>
+        void NewCandidate(string ice);
+    }
+}
--- RTCSignalingServer/Hubs/SignalingHub.cs
+++ RTCSignalingServer/Hubs/SignalingHub.cs
@@ -4,6 +4,9 @@ using Microsoft.AspNetCore.SignalR;
 
 namespace RTCSignalingServer.Hubs
 {
+    /// <summary>
+    /// Signaling hub (url: /signaling)
+    /// </summary>
     public class SignalingHub : Hub
     {
         public async Task Test()
@@ -11,6 +14,11 @@ namespace RTCSignalingServer.Hubs
             await Clients.All.InvokeAsync("FromServerTest", "Data from server");
         }
 
+        /// <summary>
+        /// Joins user to group by name.
+        /// </summary>
+        /// <param name="groupName">Name of user group (self)</param>
+        /// <returns></returns>
         public async Task JoinGroup(string groupName)
         {
             await Groups.AddAsync(Context.ConnectionId, groupName);
@@ -18,6 +26,13 @@ namespace RTCSignalingServer.Hubs
             Console.WriteLine($"{Context.ConnectionId} joined to group \"{groupName}\"");
         }
 
+        /// <summary>
+        /// Sends offer to user in specified group
+        /// </summary>
+        /// <param name="groupName">Interlocurtor group name</param>
+        /// <param name="myGroupName">User group name (own)</param>
+        /// <param name="sdp">SDP pocket for interlocutor</param>
+        /// <returns></returns>
         public async Task SendOffer(string groupName, string myGroupName, string sdp)
         {
             await Clients.Group(groupName).InvokeAsync("NewOffer", myGroupName, sdp);
@@ -25,6 +40,12 @@ namespace RTCSignalingServer.Hubs
             Console.WriteLine($"Offer from {myGroupName} to {groupName}");
         }
 
+        /// <summary>
+        /// Sends answer to user in specified group (by name)
+        /// </summary>
+        /// <param name="recipientGroup">Initiator group name</param>
+        /// <param name="sdp">SDP pocket for call initiator</param>
+        /// <returns></returns>
         public async Task SendAnswer(string recipientGroup, string sdp)
         {
             await Clients.Group(recipientGroup).InvokeAsync("NewAnswer", sdp);
@@ -32,6 +53,12 @@ namespace RTCSignalingServer.Hubs
             Console.WriteLine($"Answer to {recipientGroup}");
         }
 
+        /// <summary>
+        /// Sends candidate pocket to user in specified group (by name)
+        /// </summary>
+        /// <param name="recipientGroup">Interlocutor group name</param>
+        /// <param name="ice">ICE pocket</param>
+        /// <returns></returns>
         public async Task SendCandidate(string recipientGroup, string ice)
         {
             await Clients.Group(recipientGroup).InvokeAsync("NewCandidate", ice);
